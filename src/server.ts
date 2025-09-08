31c4c961592d1d9833b2e2755235a5abf083ef89
tektechnologies <craigwbarkley@icloud.com>
2025-09-08 18:54:43 -0500

    add capabilities - tools, resources,prompts

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
-import { McpServer } from "@modelcontextprotocol/sdk";
+import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 
 const server = new McpServer({
     name: "mcp-server-and-client",
     version: "2.0.0",
     description: "A MCP server and client",
+    capabilities: {
+        tools: {},
+        resources: {},
+        prompts: {},
+    },
 });
 
-server.tool({
-    name: "mcp-server-and-client",
-});
